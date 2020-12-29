5070dcbabac432e80def1339938e9748961bedca
jane901201 <jane36742715@gmail.com>
2020-12-29 15:27:41 +0800

    second version

--- src/com/td/VendingMachine.java
+++ src/com/td/VendingMachine.java
@@ -1,12 +1,58 @@
 package com.td;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class VendingMachine {
 
 	public static void main(String[] args) {
+		
+		//InnerSystem m_InnerSystem = new InnerSystem();
+		
+		//m_InnerSystem.getStart();
+		
+		
+		
+		
 		// TODO Auto-generated method stub
+		/*
+		String s;
+		String s2 = "aaa";
+		
+		int test[] = {1,2,3};
+		int[] name;
+		
+		name = new int[3];
+		
+		System.out.println(s2.length());
+		System.out.println(s2.startsWith("a"));
+		try {
+			System.out.println(s2.substring(4));
+		} catch(StringIndexOutOfBoundsException e) {
+			
+		}
+		
+		
+		s = null;
+		try {
+			System.out.println(s.length());
+		} catch(NullPointerException e) {
+			
+		}
+		
+		try {
+			FileReader reader = new FileReader("data.txt");
+			
+		} catch(FileNotFoundException e) {
+			System.out.println("Not found");
+		}
+		
+		
+		
 		
 		System.out.println("hh");
 		
+		*/
 	}
 
 }
