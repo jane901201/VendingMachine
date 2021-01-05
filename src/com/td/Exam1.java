d44f2c7a5189457249e870e804784f1674eefc06
User <User@ES301N20>
2021-01-05 16:32:12 +0800

    four version

---
+++ src/com/td/Exam1.java
@@ -0,0 +1,21 @@
+package com.td;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Exam1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			FileReader fr = new FileReader("d1");
+		} catch(FileNotFoundException e) {
+			
+		}
+		
+		
+		
+	}
+
+}
